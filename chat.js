5e4a1c600995741fe005ec2a2dd3f3f14e1ec26b
Adrian Victor <adrianvictor@disroot.com>
2024-11-12 11:52:39 -0300

    initial

---
+++ chat.js
@@ -0,0 +1,65 @@
+const url = new URL(window.location.href)
+const queryString = url.search
+const params = new URLSearchParams(queryString)
+let wsAddr = params.get('server') // "ws://localhost:4000" - ?server=ws%3A%2F%2Flocalhost%3A4000&nick=adrian
+const ws = new WebSocket(wsAddr)
+const main = document.querySelector('main')
+const sendButton = document.querySelector('#sendButton')
+let nick = 'unknown'
+const nickbox = document.querySelector('#username')
+let incomingNicknames = document.querySelectorAll('.incomingNickname')
+const composeBox = document.querySelector('#composeBox')
+
+function showMessage(owner, message, nickname, timestamp) { 
+    const isOwner = nick == nickname
+    let html = `<div class="message ${isOwner ? "owner" : ""}">
+    <div class="content">
+    <p><a class="incomingNickname">${nickname}</a></p>
+    ${message}
+    </div>
+    <div class="time">
+    ${timestamp}
+    </div>
+    </div>`
+    main.innerHTML += html
+    attachNicknameListeners();
+}
+
+function attachNicknameListeners() {
+    const incomingNicknames = document.querySelectorAll('.incomingNickname');
+    incomingNicknames.forEach(node => {
+        node.addEventListener('click', () => {
+            composeBox.value +=`@${node.textContent} `;
+        });
+    });
+}
+
+ws.addEventListener('open', () => {
+    console.log("Conectado")
+    if(params.get('nick') !== null) {
+        nick = params.get('nick')
+        nickbox.textContent = `${nick} @ ${wsAddr}`
+        ws.send(`/nick ${params.get('nick')}`)
+    } else {
+        nickbox.textContent = "Please chose a nickname"
+    }
+})
+
+ws.addEventListener('close', () =>{
+    console.log("Desconectado")
+    showMessage(false, "Desconectado do servidor", "Client")
+})
+
+ws.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data)
+    showMessage(false, data.message, data.nick)
+})
+
+sendButton.addEventListener('click', () => {
+    if(composeBox.value.startsWith('/nick')) {
+        nick = composeBox.value.split(' ')[1]
+        nickbox.textContent = `${nick} @ ${wsAddr}`;
+    }
+    ws.send(composeBox.value)
+    console.log(composeBox.value)
+})
